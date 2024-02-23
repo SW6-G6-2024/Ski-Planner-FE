2c07a7cae603172bf69b7922b26b202e2503643a
MortenMunk <mortenmunk97@gmail.com>
2024-02-23 23:51:12 +0100

    added animation

--- src/components/legend/LegendCard.jsx
+++ src/components/legend/LegendCard.jsx
@@ -1,6 +1,6 @@
 const LegendCard = ({name, icon}) => {
   return (
-    <div className="flex legend-card flex-row items-center">
+    <div className="transition-all duration-300 ease-in-out flex legend-card flex-row items-center">
       <img src={icon} alt="icon" className="size-8 pr-2" />
       <div className="text-white cursor-default">{name}</div>
     </div>
--- src/components/legend/MapLegend.jsx
+++ src/components/legend/MapLegend.jsx
@@ -1,35 +1,38 @@
 import React, { useState } from "react";
 import LegendCard from "./LegendCard";
 import legendData from "../../data/legendData";
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import { Slide } from "@mui/material";
 
 const MapLegend = () => {
-
   const [isExpanded, setIsExpanded] = useState(false);
 
   const toggleLegend = () => {
     console.log("toggleLegend");
     setIsExpanded(!isExpanded);
-  }
+  };
 
   return (
-    <div className="flex-1 fixed z-[9999] bottom-0 left-0 bg-legendbg bg-opacity-50 rounded-tr-lg p-2 drop-shadow-lg">
-      <div className="justify-center hover:bg-legendinteract rounded-lg hover:bg-opacity-25 cursor-pointer">
-        {isExpanded ? <ExpandMoreIcon className="text-white" onClick={toggleLegend}/> : 
-        <ExpandLessIcon className="text-white" onClick={toggleLegend}/>}
-        
+    <div className="flex-1 fixed z-[9999] bottom-0 left-0 bg-legendbg bg-opacity-50 rounded-tr-lg p-2 drop-shadow-lg w-40 transition-all">
+      <div
+        className="justify-center hover:bg-legendinteract rounded-lg hover:bg-opacity-25 cursor-pointer"
+        onClick={toggleLegend}
+      >
+        {isExpanded ? (
+          <ExpandMoreIcon className="text-white" />
+        ) : (
+          <ExpandLessIcon className="text-white" />
+        )}
       </div>
-      {isExpanded ?
+      <Slide direction="up" in={isExpanded} mountOnEnter unmountOnExit>
         <div className="flex-col">
           {legendData.map((item, index) => (
             <LegendCard key={index} name={item.name} icon={item.icon} />
           ))}
         </div>
-        : null
-      }
-
+      </Slide>
     </div>
   );
-}
+};
 export default MapLegend;
