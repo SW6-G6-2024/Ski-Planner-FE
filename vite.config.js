0f1ba9431b52549a5523f16587d4d25d308e971b
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2024-04-09 13:22:02 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import mkcert from 'vite-plugin-mkcert';
 export default defineConfig({
   plugins: [react(), mkcert({
 		savePath: "./certs/",
+		source: "github"
 	})],
 	base: "",
 	envDir: "./config/",
