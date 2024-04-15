00cdfeddfa436cb22d8dd95c41f81ac5900cca61
Mafusn <Magnus.peetz.holt@gmail.com>
2024-04-15 15:09:40 +0200

    Only fetches pistes and lifts two times

--- src/components/SkiMap.jsx
+++ src/components/SkiMap.jsx
@@ -206,7 +206,6 @@ const SkiMapComponent = () => {
         scrollWheelZoom={false}
         whenReady={(mapEvent) => {
           mapRef.current = mapEvent.target;
-          updateBoundsAndFetchData();
         }}
       >
         <TileLayer
